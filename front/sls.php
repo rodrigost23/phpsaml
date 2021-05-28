a230662310b38186f7bd9f4fafa2845c83dd0eae
Rodrigo Tavares <rodrigost23@gmail.com>
2021-05-28 17:02:44 -0400

    Preliminary SLO (only works if called from browser)

---
+++ front/sls.php
@@ -0,0 +1,39 @@
+<?php
+if (!defined('GLPI_ROOT')) {
+    define('GLPI_ROOT', '../../..');
+}
+
+if (!empty($_POST['SAMLRequest'])) {
+    $_GET['SAMLRequest'] = $_POST['SAMLRequest'];
+}
+
+$post = $_POST;
+unset($_POST);
+
+include(GLPI_ROOT . '/inc/includes.php');
+
+require_once GLPI_ROOT . '/plugins/phpsaml/lib/xmlseclibs/xmlseclibs.php';
+$libDir = GLPI_ROOT . '/plugins/phpsaml/lib/php-saml/src/Saml2/';
+
+$folderInfo = scandir($libDir);
+
+foreach ($folderInfo as $element) {
+    if (is_file($libDir . $element) && (substr($element, -4) === '.php')) {
+        require_once $libDir . $element;
+    }
+}
+
+$phpsaml = new PluginPhpsamlPhpsaml();
+
+$phpsaml::auth();
+
+$phpsaml::$auth->processSLO();
+
+$errors = $phpsaml::$auth->getErrors();
+
+if (empty($errors)) {
+    $phpsaml::glpiLogout();
+} else {
+    echo $implode(', ', $errors);
+    Toolbox::logInFile("php-errors", $implode(', ', $errors) . "\n", true);
+}
